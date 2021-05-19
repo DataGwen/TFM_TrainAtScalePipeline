54fa5ccab6c2660a41e693f000fe90bfd6e6cb85
Gwenael Vanet <gwenael.vanet@gmail.com>
2021-05-19 14:17:43 +0200

    repo create for GCP

---
+++ TaxiFareModelCloud/data.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+AWS_BUCKET_PATH = "s3://wagon-public-datasets/taxi-fare-train.csv"
+BUCKET_NAME = 'data-589-datagwen'
+BUCKET_TRAIN_DATA_PATH = 'data/train_1k.csv'
+
+
+# def get_data(nrows=10_000):
+#     '''returns a DataFrame with nrows from s3 bucket'''
+#     df = pd.read_csv(AWS_BUCKET_PATH, nrows=nrows)
+#     return df
+
+def get_data():
+    """method to get the training data (or a portion of it) from google cloud bucket"""
+    df = pd.read_csv(f"gs://{BUCKET_NAME}/{BUCKET_TRAIN_DATA_PATH}", nrows=1000)
+    #df = pd.read_csv("gs://data-589-datagwen/data/train_1k.csv", nrows=1000)
+    return df
+
+def clean_data(df, test=False):
+    df = df.dropna(how='any', axis='rows')
+    df = df[(df.dropoff_latitude != 0) | (df.dropoff_longitude != 0)]
+    df = df[(df.pickup_latitude != 0) | (df.pickup_longitude != 0)]
+    if "fare_amount" in list(df):
+        df = df[df.fare_amount.between(0, 4000)]
+    df = df[df.passenger_count < 8]
+    df = df[df.passenger_count >= 0]
+    df = df[df["pickup_latitude"].between(left=40, right=42)]
+    df = df[df["pickup_longitude"].between(left=-74.3, right=-72.9)]
+    df = df[df["dropoff_latitude"].between(left=40, right=42)]
+    df = df[df["dropoff_longitude"].between(left=-74, right=-72.9)]
+    return df
+
+
+if __name__ == '__main__':
+    df = get_data()
---
+++ TaxiFareModelCloud/params.py
@@ -0,0 +1,6 @@
+AWS_BUCKET_PATH = "s3://wagon-public-datasets/taxi-fare-train.csv"
+BUCKET_NAME = 'data-589-datagwen'
+BUCKET_TRAIN_DATA_PATH = 'data/train_1k.csv'
+MODEL_NAME = 'taxifaremodelcloud'
+MODEL_VERSION = 'v1'
+STORAGE_LOCATION = 'models/taxifaremodelcloud/model.joblib'
